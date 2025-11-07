f795abd310df0d74dc1b3480dfd9fe30af491551
Moisés <69405573+moisesEL@users.noreply.github.com>
2025-11-07 02:12:39 +0000

    commit final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [task, setTask] = useState("");
+    const [todo, setTodo] = useState([]);
+
+    return (
+        <div className="container">
+            <h1>todos</h1>
+
+            <div className="caja">
+                <input
+                    onChange={(e) => setTask(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter" && task.trim() !== "") {
+                            setTodo([...todo, task]);
+                            setTask("");
+                        }
+                    }}
+                    type="text"
+                    value={task}
+                    placeholder="Whats need to be done?">
+                </input>
+
+                <ul className="listas">
+                    {todo.length === 0 ? (
+                        <li> No hay tareas, añadir tareas </li>
+                    ) : null}
+
+                    {todo.map((item, index) => (
+                        <li>
+                            {item} {" "}
+                            <i onClick={() => {
+                                setTodo(
+                                    todo.filter(
+                                        (t, currentIndex) => index !== currentIndex
+                                    )
+                                )
+                            }
+
+                            } > <FontAwesomeIcon icon={faCircleXmark} /> </i>
+                        </li>
+                    ))}
+
+                </ul>
+                <div className="contador"> {todo.length} task left</div>
+            </div>
+        </div >
+    );
+
 };
+
+
 
 export default Home;
